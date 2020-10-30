4b8fa3e2286d01056263569ce66d0daff897f60d
Steve Hart <stevenrhart@gmail.com>
2020-10-30 23:39:07 +0000

    simplified code to a single python file that generates qubo, ising or bqm solution based on user input

--- max_cut.py
+++ max_cut.py
@@ -12,134 +12,120 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## ------- import packages -------
-# Import networkx for graph tools
+import sys
 import networkx as nx
-
-# Import dwave_networkx for d-wave graph tools/functions
 import dwave_networkx as dnx
-
-# Import dwave.system packages for the QPU
 from dwave.system import DWaveSampler, EmbeddingComposite
+from dimod import BinaryQuadraticModel
 
 # Import matplotlib.pyplot to draw graphs on screen
 import matplotlib
 matplotlib.use("agg")
 import matplotlib.pyplot as plt
 
-def create_graph(edges):
-    """Returns a graph based on the specified list of edges.
-
-    Args:
-        edges(list of tuples): each tuple represents an edge in the graph
+def get_qubo():
+    """Returns a dictionary representing a QUBO.
     """
-    # Create empty graph
-    G = nx.Graph()
+    # Create QUBO based on min(SUM(-xi - xj + 2xi*xj))
+    Q = {}
+    for i, j in G.edges:
+        Q[(i, j)] = 2  # Add quadratic terms
+    for i in G.nodes:
+        Q[(i, i)] = (-1 * d[i])  # Add linear terms
 
-    # Add edges to graph - this also adds the nodes
-    G.add_edges_from(edges)
+    return Q
 
-    return G
-
-def get_ising(nodes, edges):
+def get_ising():
     """Returns a dictionary representing the Ising formulation.
-
-    Args:
-        nodes(list of integers): nodes for the graph
-        edges(list of tuples): each tuple represents an edge in the graph
     """
-    # Create dict to track number of edges per node
-    d = dict.fromkeys(nodes, 0)
-
     # Create empty Ising dicts for h and J
     h = {}
     J = {}
     
     # Populate Ising representation
     for u, v in G.edges:
-        J[(u, v)] = 0.5 
-        
-        # Populate d for number of edges per node
-        if u in d:
-            d[u] += 1
-        if v in d:
-            d[v] += 1
-
+        J[(u, v)] = 0.5
     for n in G.nodes:
         h[n] = 0 
 
     return h, J
 
-def run_on_qpu(h, J, sampler, chain_strength, num_reads):
-    """Runs the Ising problem on the sampler provided.
-
-    Args:
-        h(dict): a representation of the linear terms of the ising problem
-        J(dict): a representation of the quadratic terms of the ising problem
-        sampler(dimod.Sampler): a sampler that uses the QPU
-        chain_strength(int): the chainstrength value to use in the sampler
-        num_reads(int): the number of reads to use in the sampler 
-    """
-    sample_set = sampler.sample_ising(h, J, chain_strength=chainstrength, num_reads=num_reads)
+def get_bqm(Q):
+    """Returns a bqm representation of the problem.
     
-    return sample_set
+    Args:
+        Q(qubo): dictionary representing a QUBO
+    """
+    # Convert to bqm
+    bqm = BinaryQuadraticModel.from_qubo(Q)
+
+    return bqm
 
 
 ## ------- Main program -------
 if __name__ == "__main__":
 
-    # # Test Graph 0 (solution = 2, 1)
+    # # Small Test Graph (solution = 2, 1)
     # nodes = [0, 1, 2]
     # edges = [(0, 1), (1, 2)]
 
-    # # Test Graph 1 (solution = 2, 3)
-    # nodes = [0, 1, 2, 3, 4]
-    # edges = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 4), (3, 4)]
-
-    # # Test Graph 2
-    # nodes = [0, 1, 2]
-    # edges = [(0, 1), (0, 2), (1, 2)]
-
-    # Test Graph 4 (solution = TBD)
+    # Large Test Graph 4 (solution = 30)
     nodes = list(i for i in range(24))
     edges = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), (6, 7), (7, 8), (8, 9), (9, 10), (10, 11), (0, 11), 
     (0, 12), (1, 13), (2, 14), (3, 15), (4, 16), (5, 17), (6, 18), (7, 19), (8, 20), (9, 21), (10, 22), (11, 23),
     (12, 16), (12, 20), (13, 17), (13, 21), (14, 18), (14, 22), (15, 19), (15, 23), (16, 20), (17, 21), (18, 22), (19, 23)]
     
-    # Create graph
-    G = create_graph(edges)
+    # Create graph as defined above
+    G = nx.Graph()
+    G.add_edges_from(edges)
 
-    # Get Ising formulation
-    h, J = get_ising(nodes, edges)
+    # Create dictionary for edges per node
+    d = dict.fromkeys(G.nodes, 0)
+    for i, j in G.edges:
+        if i in d:
+            d[i] += 1
+        if j in d:
+            d[j] += 1
 
-    # Set chainstrength & num_reads
-    chainstrength = 3
-    num_reads = 10 # update
+    # Set formulation type based on user input
+    formulation = sys.argv[1]
 
-    
-    # Define the sampler and run the problem
+    # Set parameters to run the problem
+    chainstrength = 3 # update as needed
+    num_reads = 10 # update as needed
     sampler = EmbeddingComposite(DWaveSampler())
-    sample_set = run_on_qpu(h, J, sampler, chainstrength, num_reads)
-    
-   # Print the solution
-    # print(sample_set)
-    result = list(sample_set.first.sample[i] for i in nodes)
-    set_1 = []
-    set_2 = []
+
+    # Formulate and run the problem based on command line input
+    if formulation == "qubo":
+        Q = get_qubo()
+        sample_set = sampler.sample_qubo(Q, chain_strength=chainstrength, num_reads=num_reads)
+    elif formulation == "ising":
+        h, J = get_ising()
+        sample_set = sampler.sample_ising(h, J, chain_strength=chainstrength, num_reads=num_reads)
+    elif formulation == "bqm":
+        Q = get_qubo()
+        bqm = get_bqm(Q)
+        sample_set = sampler.sample(bqm)
+
+    # Determine the resulting sets
+    result = list(sample_set.first.sample[i] for i in G.nodes)
+    set_1, set_2 = [], []
     for i in range(len(result)):
         if result[i] == 1:
             set_1.append(i)
         else:
             set_2.append(i)
-    print('The maximum cut is achieved by dividing into sets of length', len(set_1), 'and', len(set_2))
-
+    
     # Calculate max_cuts
     max_cuts = 0
-    for (i, j) in edges:
+    for (i, j) in G.edges:
         if i in set_1 and j in set_2:
             max_cuts += 1
         elif j in set_1 and i in set_2:
             max_cuts += 1
+    
+    # Print the solution
+    print('The maximum cut is achieved by dividing into sets of length', len(set_1), 'and', len(set_2))
     print('The maximum number of cuts is ', max_cuts)
     print(set_1, set_2)
 
@@ -156,7 +142,6 @@ if __name__ == "__main__":
     plt.savefig(original_name, bbox_inches='tight')
 
     # Save solution graph
-    # Note: red nodes are in the set, blue nodes are not
     solution_name = "graph_solution.png"
     nx.draw_networkx(subset_1, pos=pos, with_labels=True, node_color='r', font_color='k')
     nx.draw_networkx(subset_0, pos=pos, with_labels=True, node_color='b', font_color='w')
